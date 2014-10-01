1e5060a72622dcfd95b6f1234e2f81c945f0eb26
half-ogre <nemo@half-ogre.com>
2014-09-30 17:48:47 -0700

    add Convert fn

---
+++ Functions/Convert.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharDown
+{
+    public static partial class Functions
+    {
+        public static void Convert(string markdownPath, string pdfPath)
+        {
+            var markdownFile = new FileInfo(markdownPath);
+
+            var sourceHasMarkdownExtension = markdownFile.Extension.Equals(".md", StringComparison.InvariantCultureIgnoreCase)
+                || markdownFile.Extension.Equals(".markdown", StringComparison.InvariantCultureIgnoreCase);
+            if (!sourceHasMarkdownExtension)
+            {
+                Console.WriteLine("Source must be an .md or .markdown file.");
+                Environment.Exit(2);
+            }
+            
+            var markdownFileExists = File.Exists(markdownFile.FullName);
+            if (!markdownFileExists)
+            {
+                Console.WriteLine("Source file does not exist.");
+                Environment.Exit(3);
+            }
+
+            var pdfFile = new FileInfo(pdfPath);
+
+            var destinationHasPdfExtension = pdfFile.Extension.Equals(".pdf", StringComparison.InvariantCultureIgnoreCase);
+            if (!destinationHasPdfExtension)
+            {
+                Console.WriteLine("Destination must be a .pdf file.");
+                Environment.Exit(4);
+            }
+
+            var pdfFileExists = File.Exists(pdfFile.FullName);
+            if (pdfFileExists)
+            {
+                Console.WriteLine("Destination file already exists.");
+                Environment.Exit(5);
+            }
+
+            var character = ReadMarkdown(markdownFile.FullName);
+            WritePdf(character, pdfFile.FullName);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,5 @@
-﻿using iTextSharp.text.pdf;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CharDown
 {
@@ -24,26 +19,8 @@ namespace CharDown
             var source = args[0];
             var destination = args[1];
 
-            var sourceFileInfo = new FileInfo(source);
-            if (sourceFileInfo.Extension.Equals(".pdf", StringComparison.InvariantCultureIgnoreCase)
-                && File.Exists(sourceFileInfo.FullName))
-            {
-                var character = fn.ReadPdf(source);
-                fn.WriteMarkdown(character, destination);
-            }
-            else if (sourceFileInfo.Extension.Equals(".md", StringComparison.InvariantCultureIgnoreCase)
-                || (sourceFileInfo.Extension.Equals(".markdown", StringComparison.InvariantCultureIgnoreCase))
-                && File.Exists(sourceFileInfo.FullName))
-            {
-                var character = fn.ReadMarkdown(source);
-                fn.WritePdf(character, destination);
-            }
-            else
-            {
-                Console.WriteLine("Source must be a .pdf, .md, or .markdown file.");
-                Environment.Exit(2);
-            }
-
+            fn.Convert(source, destination);
+            
             if (Debugger.IsAttached)
             {
                 Console.WriteLine("Press ENTER to quit.");
